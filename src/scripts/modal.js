e995795c57654b3ff5f86279ddfacbe33aa1a6b8
LaisNardes <lais.nardes0398@gmail.com>
2024-09-01 06:17:50 -0400

    Login model using modal and dialog
    The purpose of this activity is to develop a simple but efficient modal using dialog element. Also a js code to make the dialog interact with users.

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,22 @@
 /* Desenvolva seu código aqui... */
+function modalRegister (){
+    const catchLink = document.querySelector('.form__register-link')
+    const catchModal = document.querySelector('#modalController')
+
+    catchLink.addEventListener('click', () =>{
+        catchModal.showModal()
+
+        closingModalButton()
+})
+}
+
+function closingModalButton (){
+    const button = document.querySelector('.toCloseModal')
+    const catchModal = document.querySelector('#modalController')
+
+    button.addEventListener('click', () => {
+        modalController.close()
+})
+}
+
+ modalRegister()
